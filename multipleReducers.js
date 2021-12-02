3711583cb79234e86666247003078491d0f9442a
aamna-sys <aamna.sayyed@systemsltd.com>
2021-12-02 17:10:42 +0500

    Combine reducers

--- multipleReducers.js
+++ multipleReducers.js
@@ -1,5 +1,6 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
 
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICECREAM";
@@ -52,6 +53,18 @@ const iceCreamReducer = (state = initialIceCreamState, action) => {
       return state;
   }
 };
+
+// Combine reducers
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+
+const store = createStore(rootReducer);
+console.log("Initial state:", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated state:", store.getState())
+);
 
 store.dispatch(buyCake());
 store.dispatch(buyCake());
