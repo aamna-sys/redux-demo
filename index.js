5d6bbc855058535b67663e39a454aff6412347b9
aamna-sys <aamna.sayyed@systemsltd.com>
2021-12-02 15:24:18 +0500

    Add reducer function

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ function buyCake() {
     // you can have any number of properties you want
   };
 }
+
+// reducer function needs state and action as parameters
+
+//state
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    default:
+      return state;
+  }
+};
